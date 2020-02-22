f9f8a7db200d4b821b9c267567128eb4fb4e533d
Anil Onal <anilca@gmail.com>
2020-02-21 23:31:06 -0500

    training

--- .ipynb_checkpoints/data_prep-checkpoint.py
+++ .ipynb_checkpoints/data_prep-checkpoint.py
@@ -7,6 +7,7 @@ The file cleans...
 """
 import pandas as pd
 import os
+from sklearn.preprocessing import OneHotEncoder
 
 def select_feats():
     """
@@ -59,3 +60,17 @@ def select_feats():
            student_characteristics_df, \
            parental_involvement_df, \
            X_cont_labels
+
+# Function to transform categorical variables
+def encoder_transform(encoder, X):
+    X_encoded = encoder.transform(X).toarray()
+    encoded_feats = list(encoder.get_feature_names())
+    feats = X.columns
+    encoded_feats_updated = []
+    for feat in encoded_feats:
+        feat_split = feat.split('_')
+        i = int(feat_split[0][1:])
+        dummies = feat_split[1]
+        feat_updated = f'{feats[i]}_{dummies}'
+        encoded_feats_updated.append(feat_updated)
+    return pd.DataFrame(X_encoded, columns = encoded_feats_updated)
